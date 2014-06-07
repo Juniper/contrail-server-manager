4bbd2b557e3a52a6469117d2e2ac393baed6fb17
Prasad Miriyala <pmiriyala@centos-build01.(none)>
2014-06-06 18:58:37 -0700

    Modify client ini file location to the installed dir

--- src/client/smgr_client_def.py
+++ src/client/smgr_client_def.py
@@ -2,5 +2,8 @@
 
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
+import os
+
 _DEF_SMGR_PORT = 9001
-_DEF_SMGR_CFG_FILE = "./smgr_client_config.ini"
+_DEF_SMGR_CFG_FILE = os.path.dirname(__file__) + "/smgr_client_config.ini"
+
