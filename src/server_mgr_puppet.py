a872248fa4763f870bc43098626d198334b5182f
tsurendra <tsurendra@juniper.net>
2014-07-09 18:01:04 -0700

    [1339941] ESX related changes. Fix HA-proxy staging dir

--- src/server_mgr_puppet.py
+++ src/server_mgr_puppet.py
@@ -14,6 +14,7 @@ import string
 import textwrap
 import shutil
 from server_mgr_logger import ServerMgrlogger as ServerMgrlogger
+from esxi_contrailvm import ContrailVM as ContrailVM
 
 class ServerMgrPuppet:
     _puppet_site_file_name = "site.pp"
@@ -336,8 +337,8 @@ class ServerMgrPuppet:
 
 
     def create_config_ha_proxy(self, provision_params):
-        smgr_dir = "/etc/puppet/modules/contrail_"+ provision_params['puppet_manifest_version'] + "/files/"
-        staging_dir = "/etc/puppet/modules/contrail-common/files/"
+	smgr_dir  = staging_dir = "/etc/puppet/modules/contrail_"+ provision_params['puppet_manifest_version'] + "/files/"
+
         cfg_ha_proxy_tmpl = string.Template("""
 #contrail-config-marker-start
 listen contrail-config-stats :5937
@@ -381,8 +382,7 @@ $__contrail_disc_backend_servers__
         api_server_lines = ''
         disc_listen_port = 9110
         disc_server_lines = ''
-        smgr_dir = "/etc/puppet/modules/contrail_"+ provision_params['puppet_manifest_version'] + "/files/"
-        staging_dir = "/etc/puppet/modules/contrail-common/files/"
+	smgr_dir  = staging_dir = "/etc/puppet/modules/contrail_"+ provision_params['puppet_manifest_version'] + "/files/"
         #TODO
         nworkers = 1
         for config_host in config_role_list:
@@ -718,8 +718,7 @@ $__contrail_disc_backend_servers__
     #Function to create haproxy cfg file for compute nodes
     def create_compute_ha_proxy(self, provision_params):
         
-        smgr_dir = "/etc/puppet/modules/contrail_"+ provision_params['puppet_manifest_version'] + "/files/"
-        staging_dir = "/etc/puppet/modules/contrail-common/files/"
+	smgr_dir  = staging_dir = "/etc/puppet/modules/contrail_"+ provision_params['puppet_manifest_version'] + "/files/"
 
         compute_haproxy_template = string.Template("""
 #contrail-compute-marker-start
@@ -858,8 +857,8 @@ $__contrail_glance_apis__
 
     #Function to create haproxy cfg for openstack nodes
     def create_openstack_ha_proxy(self, provision_params):
-        smgr_dir = "/etc/puppet/modules/contrail_"+ provision_params['puppet_manifest_version'] + "/files/"
-        staging_dir = "/etc/puppet/modules/contrail-common/files/"
+	smgr_dir  = staging_dir = "/etc/puppet/modules/contrail_"+ provision_params['puppet_manifest_version'] + "/files/"
+
         openstack_haproxy_template = string.Template("""
 #contrail-openstack-marker-start
 listen contrail-openstack-stats :5936
@@ -930,6 +929,36 @@ $__contrail_quantum_servers__
     def puppet_add_compute_role(self, provision_params, last_res_added):
         # Get all the parameters needed to send to puppet manifest.
         data = ''
+        #Vm on top of ESX Server
+        if 'esx_server' in provision_params.keys():
+            print "esx_server"
+            #call scripts to provision esx
+                vm_params = {}
+                vm_params['vm'] = "ContrailVM"
+                vm_params['vmdk'] = "ContrailVM"
+                vm_params['datastore'] = provision_params['datastore']
+                vm_params['eth0_mac'] = provision_params['server_mac']
+                vm_params['eth0_ip'] = provision_params['server_ip']
+                vm_params['eth0_pg'] = provision_params['esx_fab_port_group']
+                vm_params['eth0_vswitch'] = provision_params['esx_fab_vswitch']
+                vm_params['eth0_vlan'] = None
+                vm_params['eth1_vswitch'] = provision_params['esx_vm_vswitch']
+                vm_params['eth1_pg'] = provision_params['esx_vm_port_group']
+                vm_params['eth1_vlan'] = "4095"
+                vm_params['uplink_nic'] = provision_params['esx_uplink_nic']
+                vm_params['uplink_vswitch'] = provision_params['esx_fab_vswitch']
+                vm_params['server'] = provision_params['esx_ip']
+                vm_params['username'] = provision_params['esx_username']
+                vm_params['passwd'] = provision_params['esx_passwd']
+                vm_params['thindisk'] =  provision_params['esx_vmdk']
+                vm_params['smgr_ip'] = provision_params['smgr_ip'];
+                vm_params['domain'] =  provision_params['domain']
+                vm_params['vm_passwd'] = provision_params['passwd']
+                vm_params['vm_server'] = provision_params['server_id']
+                vm_params['vm_deb'] = provision_params['vm_deb']
+                out = ContrailVM(vm_params)
+                print out
+
         control_servers = provision_params['roles']['control']
         #control_servers_control = self.get_control_ip(provision_params,control_servers)
 
@@ -995,7 +1024,10 @@ $__contrail_quantum_servers__
         contrail_ks_admin_passwd => "%s",
         contrail_ks_admin_tenant => "%s",
         contrail_haproxy => "%s",
-
+        contrail_vm_ip => "%s",
+        contrail_vm_username => "%s",
+        contrail_vm_passwd => "%s",
+        contrail_vswitch => "%s",
         require => %s
     }\n\n''' % (
         provision_params['puppet_manifest_version'],
@@ -1008,7 +1040,11 @@ $__contrail_quantum_servers__
         non_mgmt_gw,
         provision_params["ks_user"], provision_params["ks_passwd"],
         provision_params["ks_tenant"], provision_params["haproxy"],
-        last_res_added)
+        provision_params["esx_ip"],
+        provision_params["esx_username"], provision_params["esx_passwd"],
+        provision_params["esx_vm_vswitch"],
+	    last_res_added)
+
 
 
         if provision_params["haproxy"] == "enable":
