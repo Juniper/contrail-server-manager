6bf28d4887fcb014cb7d4a12d6ac366f9187e399
Chandan Mishra <cdmishra@juniper.net>
2014-08-22 17:15:15 -0700

    bug fix for bugs 1355006, 1354579, 1354581, 1355712, 1354579

--- src/puppet/modules/contrail-database/manifests/init.pp
+++ src/puppet/modules/contrail-database/manifests/init.pp
@@ -5,7 +5,7 @@ define database-template-scripts {
     # Ensure template param file is present with right content.
     file { "/etc/contrail/${title}" : 
         ensure  => present,
-	before => Service["supervisord-contrail-database"],
+	before => Service["supervisor-database"],
         content => template("contrail-database/${title}.erb"),
     }
 }
@@ -35,7 +35,7 @@ define contrail-database (
     }
 
     if ($operatingsystem == "Ubuntu"){
-        file {"/etc/init/supervisord-contrail-database.override": ensure => absent, require => Package['contrail-openstack-database']}
+        file {"/etc/init/supervisor-database.override": ensure => absent, require => Package['contrail-openstack-database']}
     }
 
     # database venv installation
@@ -76,14 +76,14 @@ define contrail-database (
     # Below is temporary to work-around in Ubuntu as Service resource fails
     # as upstart is not correctly linked to /etc/init.d/service-name
     if ($operatingsystem == "Ubuntu") {
-        file { '/etc/init.d/supervisord-contrail-database':
+        file { '/etc/init.d/supervisor-database':
             ensure => link,
             target => '/lib/init/upstart-job',
-            before => Service["supervisord-contrail-database"]
+            before => Service["supervisor-database"]
         }
     }
     # Ensure the services needed are running.
-    service { "supervisord-contrail-database" :
+    service { "supervisor-database" :
         enable => true,
         require => [ Package["contrail-openstack-database"],
                      Exec['database-venv'] ],
