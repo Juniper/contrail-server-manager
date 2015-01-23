b818a9ca82520b66d92d3bc2883318a2db6966fd
Dheeraj Gautam <dgautam@juniper.net>
2015-01-22 21:35:56 -0800

    Generate storage values even if storage is not configured.
    Unit Test:
    added a server without storage roles, provisioned server, verified that
    provisioned started
    
    Change-Id: I4cfcb7769bf6ebb250d07ab2fa0deb1b8603866d
    Closes-bug:1413832

--- src/server_mgr_puppet.py
+++ src/server_mgr_puppet.py
@@ -1844,6 +1844,21 @@ $__contrail_quantum_servers__
             if 'storage_server_disks' in provision_params:
                 storage_disks = [  x.encode('ascii') for x in provision_params['storage_server_disks']]
                 data += 'contrail::params::storage_osd_disks: %s\n' %(str(storage_disks))
+        else:
+            data += 'contrail::params::host_roles: []\n'
+            data += 'contrail::params::storage_num_osd: ""\n'
+            data += 'contrail::params::storage_fsid: ""\n'
+            data += 'contrail::params::storage_num_hosts: 0\n'
+            data += 'contrail::params::storage_virsh_uuid: ""\n'
+            data += 'contrail::params::storage_monitor_secret: ""\n'
+            data += 'contrail::params::storage_admin_key: ""\n'
+            data += 'contrail::params::osd_bootstrap_key: ""\n'
+            data += 'contrail::params::storage_enabled: ""\n'
+            data += 'contrail::params::live_migration_storage_scope: ""\n'
+            data += 'contrail::params::live_migration_host: ""\n'
+            data += 'contrail::params::storage_monitor_hosts: []\n'
+            data += 'contrail::params::storage_osd_disks: []\n'
+            
 
         with open(hiera_filename, "w") as site_fh:
             site_fh.write(data)
