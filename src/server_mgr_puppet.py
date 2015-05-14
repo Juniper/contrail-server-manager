c3573ff162dc2eb15eb86b59a527fa74d7686f3a
Abhay Joshi <abhayj@juniper.net>
2015-05-14 15:30:55 -0700

    Closes-Bug: #1455253     Additional parameter in cluster.json to be passed to database.pp (for contrail-database-nodemgr.conf template).
    Change-Id: I507af76649aae04e49a636e274ddfad75f60025f

--- src/server_mgr_puppet.py
+++ src/server_mgr_puppet.py
@@ -1824,6 +1824,7 @@ $__contrail_quantum_servers__
             "database_dir" : ["database_dir", "string"],
             "analytics_data_dir" : ["analytics_data_dir", "string"],
             "ssd_data_dir" : ["ssd_data_dir", "string"],
+            "database_minimum_diskGB" : ["database_minimum_diskGB", "integer"],
             "keystone_ip" : ["keystone_ip", "string"],
             "keystone_password" : ["keystone_admin_password", "string"],
             "service_token" : ["keystone_service_token", "string"],
