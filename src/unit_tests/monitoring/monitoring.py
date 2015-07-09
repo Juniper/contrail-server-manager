a0de73053dc7abfae9d2ae7ce45250f40c787876
sgurumurthy <sgurumurthy@juniper.net>
2015-07-09 16:09:30 -0700

    Related-Bug: #1467647 Added one test case for a function using the ssh command
    Change-Id: I39dc31bff77e8de750fa0f28de7dd1485467c2a9

--- src/unit_tests/monitoring/monitoring.py
+++ src/unit_tests/monitoring/monitoring.py
@@ -8,6 +8,8 @@ from server_mgr_logger import ServerMgrlogger as ServerMgrlogger
 from server_mgr_ssh_client import ServerMgrSSHClient
 import verify_result
 
+package_installed = True
+
 #This is the class for mocking Server Manager logging	 
 class mock_logger(object):
     def __init__(self, *args, **kwargs):
@@ -15,10 +17,20 @@ class mock_logger(object):
 
 #Mock class for ServerManager SSH client connection
 class mock_ServerMgrSSHClient(object):
+    filename = None
     def __init__(self, *args, **kwargs):
         return
     def exec_command(self, cmd):
-        print cmd
+        if cmd == 'which iostat' and package_installed == False:
+            return None
+        elif cmd == 'which iostat' and package_installed == True:
+            return "package_installed"
+	elif cmd == 'iostat -m':
+            file_name = './monitoring/test-data/' + self.filename
+        
+        f = open(file_name, 'r')
+        result= f.read()
+        return result
 
 #Class for testing the entire monitoring functionality	
 class TestMonitoring(unittest.TestCase):
@@ -27,7 +39,7 @@ class TestMonitoring(unittest.TestCase):
     delayed_response = False
     ipmonitor = None
     raiseException = False
-
+    
     #Mock function for the call_subprocess	
     def mock_subprocess(self,cmd):
         file_name = './monitoring/test-data/' + self.filename
@@ -49,9 +61,13 @@ class TestMonitoring(unittest.TestCase):
                 verify_result.checkQuantaData(self,args[1])
             elif self.filename == 'sensor/supermicro_sensor_expected_output.txt':
                 verify_result.checkSuperMicroData(self,args[1])
-            elif args[3] == 'ipmi_chassis_data':
-                if self.filename == 'chassis/chassis_expected_output.txt':
-                    verify_result.checkChassisData(self,args[1])
+        elif args[3] == 'ipmi_chassis_data':
+            if self.filename == 'chassis/chassis_expected_output.txt':
+                verify_result.checkChassisData(self,args[1])
+        elif args[3] == 'disk_list_tot':
+            verify_result.checkDiskTotInfoData(self, args[1], package_installed)
+        elif args[3] == 'disk_list':
+            verify_result.checkDiskInfoData(self, args[1], package_installed)
    
     #setup function which sets up environemnt required for unit testing 
     def setUp(self):
@@ -64,11 +80,13 @@ class TestMonitoring(unittest.TestCase):
         self.ipmonitor.send_ipmi_stats = self.mock_send_ipmi_stats
 
     def tearDown(self):
+        global package_installed
         self.ipmonitor = None
         self.filename = None
         self.noresponse = False
         self.delayed_response = False
         self.raiseException = False
+        package_installed = True
 
     def testSensorData(self):
         supported_sensors = ['FAN|.*_FAN', '^PWR', '.*Temp', '.*_Power']
@@ -108,15 +126,28 @@ class TestMonitoring(unittest.TestCase):
             self.ipmonitor.fetch_and_process_chassis("dummy", "dummy", "dummy", "dummy", "dummy")
 
     def testDiskInfo(self):
+        global package_installed
         sshclient = ServerMgrSSHClient(None)
+        sshclient.filename = 'disk_info/disk_info_expected_output.txt'
         self.ipmonitor.fetch_and_process_disk_info("dummy", "dummy", sshclient)
+        #Test the case where the diff between the previous and current iteration is sent
+        sshclient.filename = 'disk_info/disk_info_diff_expected_output.txt'
+        self.ipmonitor.fetch_and_process_disk_info("dummy", "dummy", sshclient)
+        #Test the situation where the sysstat package is not installed on the target node
+        package_installed = False
+        self.ipmonitor.fetch_and_process_disk_info("dummy", "dummy", sshclient)
+        sshclient.filename = 'disk_info/disk_info_wrong_output.txt'
+        #Make sure the exception is raised. If the exception is not raised the test case will fail
+        package_installed = True
+        with self.assertRaises(Exception):
+            self.ipmonitor.fetch_and_process_disk_info("dummy","dummy",sshclient)
 
 #Create a TestSuite for the testing the entire Monitoring functionality
 def monitoring_suite():
     suite = unittest.TestSuite()
     suite.addTest(TestMonitoring('testSensorData'))
     suite.addTest(TestMonitoring('testChassisData'))
-    #suite.addTest(TestMonitoring('testDiskInfo'))
+    suite.addTest(TestMonitoring('testDiskInfo'))
     return suite
 
 #Run the Monitoring testsuite when this scrip is run
--- src/unit_tests/monitoring/verify_result.py
+++ src/unit_tests/monitoring/verify_result.py
@@ -1,5 +1,6 @@
 import unittest
 
+#Function to test the output of the expected Quanta output for ipmi sensor
 def checkQuantaData(self,output):
     self.assertEqual(output[0].sensor, 'MB1_Temp')
     self.assertEqual(output[0].sensor_type, 'temperature')
@@ -17,6 +18,7 @@ def checkQuantaData(self,output):
     self.assertEqual(output[2].unit, 'Watts')
     self.assertEqual(output[2].status, 'ok')
 
+#Function to test the output of the expected SuperMicro output for ipmi sensor
 def checkSuperMicroData(self, output):
     self.assertEqual(output[0].sensor, 'System Temp')
     self.assertEqual(output[0].sensor_type, 'temperature')
@@ -34,6 +36,7 @@ def checkSuperMicroData(self, output):
     self.assertEqual(output[2].unit, 'RPM')
     self.assertEqual(output[2].status, 'ok')
 
+#Function to test the output of the expected chassis output
 def checkChassisData(self,output):
     self.assertEqual(output.system_power, 'on')
     self.assertFalse(output.power_overload)
@@ -46,3 +49,31 @@ def checkChassisData(self,output):
     self.assertEqual(output.front_panel_lockout, 'inactive')
     self.assertFalse(output.drive_fault)
     self.assertFalse(output.cooling_fan_fault)
+
+#Function to test the output of the expected Disk Info data
+def checkDiskTotInfoData(self, output, package_installed):
+    if package_installed == False:
+        self.assertEqual(output[0].total_read_bytes, 0)
+        self.assertEqual(output[0].disk_name, None)
+        self.assertEqual(output[0].total_write_bytes, 0)
+    else:
+        self.assertEqual(output[0].total_read_bytes, 3165)
+        self.assertEqual(output[0].disk_name, 'sda')
+        self.assertEqual(output[0].total_write_bytes, 10788)
+        self.assertEqual(output[1].total_read_bytes, 1025)
+        self.assertEqual(output[1].disk_name, 'dm-0')
+        self.assertEqual(output[1].total_write_bytes, 58147)
+
+#Function to the Disk data for the diff's
+def checkDiskInfoData(self, output, package_installed):
+    if package_installed == False:
+        self.assertEqual(output[0].read_bytes, 0)
+        self.assertEqual(output[0].disk_name, 'N/A')
+        self.assertEqual(output[0].write_bytes, 0)
+    else:
+        self.assertEqual(output[0].read_bytes, 0)
+        self.assertEqual(output[0].disk_name, 'sda')
+        self.assertEqual(output[0].write_bytes, 0)
+        self.assertEqual(output[1].read_bytes, 0)
+        self.assertEqual(output[1].disk_name, 'dm-0')
+        self.assertEqual(output[1].write_bytes, 0)
