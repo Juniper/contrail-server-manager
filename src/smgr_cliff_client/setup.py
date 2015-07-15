7a8823f91836f00dc6427d7c571445db3f8b3e02
nitishkrishna <nitishk@juniper.net>
2015-07-15 15:01:42 -0700

    Related-Bug: #1463208 - Moving Server Manager Client CLI to Cliff Framework
    This check-in:
    - Added main Cliff framework files
    - Added Server Manager commands
    - Added setup.py for cliff client pip installable package
    - Packaging scripts in separate checkin
    
    Change-Id: I5b9fac1474fba58feb08588a549eec40f77542c9

---
+++ src/smgr_cliff_client/setup.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+PROJECT = 'servermanagercli'
+
+
+def get_version():
+    version = None
+    with open('contrail_version.txt') as f:
+        version = f.read()
+        version = version.strip()
+    if version:
+        return version
+    else:
+        return "1.0"
+
+
+def get_contrail_sku():
+    sku = None
+    with open('contrail_sku.txt') as f:
+        sku = f.read()
+        sku = sku.strip()
+    if sku:
+        return sku
+    else:
+        return "havana"
+
+CONTRAIL_VERSION = get_version()
+CONTRAIL_SKU = get_contrail_sku()
+
+VERSION = CONTRAIL_VERSION + "-" + CONTRAIL_SKU
+
+import setuptools
+from pip import req, download
+from pip.req import parse_requirements
+from pip.download import PipSession
+import ConfigParser
+
+install_reqs = req.parse_requirements("requirements.txt", session=download.PipSession())
+reqs = [str(ir.req) for ir in install_reqs]
+config = ConfigParser.ConfigParser()
+config.read('setup.cfg')
+
+config_dict = dict(config._sections)
+for k in config_dict:
+    config_dict[k] = dict(config._defaults, **config_dict[k])
+    config_dict[k].pop('__name__', None)
+
+console_script_list = [str(x) for x in (config_dict['entry_points']['console_scripts']).splitlines() if x and x[0] != "#"]
+entry_points_dict = dict()
+entry_points_dict['console_scripts'] = console_script_list
+
+entry_points = [key for key in (config_dict['entry_points']) if key != "console_scripts"]
+for entry_point in entry_points:
+    command_list = [str(x) for x in (config_dict['entry_points'][str(entry_point)]).splitlines() if x and x[0] != "#"]
+    entry_points_dict[str(entry_point)] = command_list
+
+
+setuptools.setup(
+    name=PROJECT,
+    version=VERSION,
+    setup_requires=['configparser'],
+    description='Server Manager Command Line Interface',
+    package_data={'': ['*.ini', '*.txt', '*.sh', '*.bash_completion']},
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=reqs,
+    platforms=['Any'],
+    entry_points=entry_points_dict,
+    scripts=['setup_smgrcliapp.sh'],
+    data_files=[
+        ('/tmp', ['smgrcliapp/sm-client-config.ini']),
+        ('/tmp', ['smgrcliapp.bash_completion'])
+    ],
+    zip_safe=False)
+
