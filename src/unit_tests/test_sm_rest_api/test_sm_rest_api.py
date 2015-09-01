befb949350cf61becd0704f6579754953b2e1db9
Ritam Gangopadhyay <ritam@juniper.net>
2015-09-01 15:53:36 +0530

    Adding unit testcases for cluster rest api's. Adding unit testcases for tag rest api's. Closes-Bug:#1467647
    Change-Id: I84dcb73dfff38bef8218fad8efda5d90bd20319c

--- src/unit_tests/test_sm_rest_api/test_sm_rest_api.py
+++ src/unit_tests/test_sm_rest_api/test_sm_rest_api.py
@@ -94,7 +94,7 @@ def launch_server_manager(
 
 #Class for testing Server manager rest API calls for server object
 class TestSMServerRestApi(unittest.TestCase):
-    #setup function which sets up environemnt required for unit testing 
+    #setup function which sets up environemnt required for unit testing
     @classmethod
     def setUpClass(cls):
         # Remove existing database and log files.
@@ -246,6 +246,273 @@ def sm_server_rest_api_suite():
     suite.addTest(TestSMServerRestApi('testDeleteServerRestApi'))
     return suite
 
+#Class for testing Server manager rest API calls for cluster object
+class TestSMClusterRestApi(unittest.TestCase):
+    #setup function which sets up environemnt required for unit testing
+    @classmethod
+    def setUpClass(cls):
+        # Remove existing database and log files.
+        try:
+            os.remove("smgr_data.db")
+            os.remove("debug.log")
+            os.remove("inventory_debug.log")
+            os.remove("monitoring_debug.log")
+            os.remove("transaction.log")
+        except:
+            pass
+        cls.server_ip = socket.gethostbyname(socket.gethostname())
+        cls.server_port = get_free_port()
+        cls._server_manager_greenlet = gevent.spawn(
+            launch_server_manager, cls.server_ip, cls.server_port,
+            "sm-config.ini")
+        block_till_port_listened(cls.server_ip, cls.server_port)
+
+    # Teardown function called after all test cases.
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    # Test case to test cluster get REST API call.
+    def testGetClusterEmptyRestApi(self):
+        # Get clusters from db and make sure list is empty
+        response = requests.get('http://%s:%s/cluster' %(self.server_ip, self.server_port))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(eval(response.content)['cluster'], [])
+    # end testGetClusterEmptyRestApi
+
+    # Test case to test cluster add REST API call.
+    def testAddClusterRestApi(self):
+        # Add clusters to DB, request adds 2 clusters. Make sure response of add is ok.
+        # then get all clusters and check response to confirm there are 2 clusters.
+        # also get a cluster by selection criteria and make sure correct cluster is returned.
+        with open('test_cluster.json') as data_file:
+            cluster_payload = data_file.read()
+        response = requests.put(
+            'http://%s:%s/cluster' %(self.server_ip, self.server_port),
+            data=cluster_payload,
+            headers = {'content-type': 'application/json'})
+        # Clusters should be added successfully.
+        self.assertEqual(response.status_code, 200)
+    # end testAddClusterRestApi
+
+    # Test case to test cluster get REST API call, after 2 clusters are added to the DB.
+    def testGetClusterAllRestApi(self):
+        response = requests.get('http://%s:%s/cluster' %(self.server_ip, self.server_port))
+        self.assertEqual(response.status_code, 200)
+        clusters = response.json()['cluster']
+        cluster_ids = [cluster['id'] for cluster in clusters]
+        self.assertEqual(len(cluster_ids), 2)
+        self.assertTrue(id in ['test-cluster-01', 'test-cluster-02'] for id in cluster_ids)
+    # end testGetClusterAllRestApi
+
+    # Test case to test cluster get REST API call for a specific cluster
+    def testGetSpecificClusterRestApi(self):
+        # Check get cluster with cluster-id for one of the clusters, should succeed
+        response = requests.get(
+            'http://%s:%s/cluster?id=%s' %(
+                self.server_ip, self.server_port, "test-cluster-01"))
+        self.assertEqual(response.status_code, 200)
+        cluster = response.json()['cluster'][0]
+        self.assertEqual(cluster['id'], 'test-cluster-01')
+    # end testGetSpecificClusterRestApi
+
+    # Test case to test cluster get REST API call for a non-existing cluster
+    def testGetNonexistingClusterRestApi(self):
+        # Check get cluster with a non-existing cluster id, should return empty response.
+        response = requests.get(
+            'http://%s:%s/cluster?id=%s' %(
+                self.server_ip, self.server_port, "nonexistingcluster"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(eval(response.content)['cluster'], [])
+    # end testGetNonexistingClusterRestApi
+
+    # Test case to test cluster modify REST API call
+    def testModifyClusterRestApi(self):
+        # Modify one of the clusters, get and ensure modification is handled correctly.
+        # When modifying, modify a column field in cluster_table, and also some parameters
+        # in fields which are stored as blob (e.g. network, parameters, etc).
+        response = requests.get(
+            'http://%s:%s/cluster?id=%s&detail=True' %(
+                self.server_ip, self.server_port, "test-cluster-01"))
+        self.assertEqual(response.status_code, 200)
+        cluster = response.json()['cluster'][0]
+        cluster['email']='test@test.com'
+        cluster['base_image_id']='test_base_image_id'
+        cluster['parameters']['router_asn']='65000'
+        cluster['parameters']['subnet_mask']='255.255.0.0'
+        cluster['parameters']['haproxy']=''
+        newdata = {"cluster":[]}
+        newdata['cluster'].append(cluster)
+        response = requests.put(
+            'http://%s:%s/cluster' %(self.server_ip, self.server_port),
+            data=json.dumps(newdata),
+            headers = {'content-type': 'application/json'})
+        self.assertEqual(response.status_code, 200)
+        response = requests.get(
+            'http://%s:%s/cluster?id=%s&detail=True' %(
+                self.server_ip, self.server_port, "test-cluster-01"))
+        self.assertEqual(response.status_code, 200)
+        cluster = response.json()['cluster'][0]
+        self.assertEqual(cluster['email'], 'test@test.com')
+        self.assertEqual(cluster['base_image_id'], 'test_base_image_id')
+        self.assertEqual(cluster['parameters']['router_asn'], '65000')
+        self.assertEqual(cluster['parameters']['subnet_mask'], '255.255.0.0')
+        self.assertEqual(cluster['parameters']['haproxy'], '')
+    # end testModifyClusterRestApi
+
+    def testDeleteClusterRestApi(self):
+        # Delete a cluster, confirm result of delete REST, then go ahead and make get call
+        # on the deleted cluster and ensure that it is deleted.
+        response = requests.delete(
+            'http://%s:%s/cluster?id=%s' %(
+                self.server_ip, self.server_port, "test-cluster-01"))
+        self.assertEqual(response.status_code, 200)
+        response = requests.get(
+            'http://%s:%s/cluster?id=%s' %(
+                self.server_ip, self.server_port, "test-cluster-01"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(eval(response.content)['cluster'], [])
+
+        response = requests.get(
+            'http://%s:%s/cluster' %(self.server_ip, self.server_port))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('test-cluster-02' in response.content)
+
+        response = requests.delete(
+            'http://%s:%s/cluster?id=%s' %(
+                self.server_ip, self.server_port, "test-cluster-02"))
+        self.assertEqual(response.status_code, 200)
+        response = requests.get(
+            'http://%s:%s/cluster?id=%s' %(
+                self.server_ip, self.server_port, "test-cluster-02"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(eval(response.content)['cluster'], [])
+    # end testDeleteClusterRESTApi
+# end class TestSMClusterRestApi
+
+#Create a TestSuite for the testing server manager rest api functionality
+def sm_cluster_rest_api_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(TestSMClusterRestApi('testGetClusterEmptyRestApi'))
+    suite.addTest(TestSMClusterRestApi('testAddClusterRestApi'))
+    suite.addTest(TestSMClusterRestApi('testGetClusterAllRestApi'))
+    suite.addTest(TestSMClusterRestApi('testGetSpecificClusterRestApi'))
+    suite.addTest(TestSMClusterRestApi('testGetNonexistingClusterRestApi'))
+    suite.addTest(TestSMClusterRestApi('testModifyClusterRestApi'))
+    suite.addTest(TestSMClusterRestApi('testDeleteClusterRestApi'))
+    return suite
+
+
+#Class for testing Server manager rest API calls for tag object
+class TestSMTagRestApi(unittest.TestCase):
+    #setup function which sets up environemnt required for unit testing
+    @classmethod
+    def setUpClass(cls):
+        # Remove existing database and log files.
+        try:
+            os.remove("smgr_data.db")
+            os.remove("debug.log")
+            os.remove("inventory_debug.log")
+            os.remove("monitoring_debug.log")
+            os.remove("transaction.log")
+        except:
+            pass
+        cls.server_ip = socket.gethostbyname(socket.gethostname())
+        cls.server_port = get_free_port()
+        cls._server_manager_greenlet = gevent.spawn(
+            launch_server_manager, cls.server_ip, cls.server_port,
+            "sm-config.ini")
+        block_till_port_listened(cls.server_ip, cls.server_port)
+
+    # Teardown function called after all test cases.
+    @classmethod
+    def tearDownClass(cls):
+        tag_data = '{"tag4": "rack", "tag5": "user_tag", "tag6": "", "tag7": "", "tag1": "datacenter", "tag2": "floor", "tag3": "hall"}'
+        response = requests.put('http://%s:%s/tag' %(
+            cls.server_ip, cls.server_port), data=tag_data, headers = {'content-type': 'application/json'})
+
+    # Test case to test tag get REST API call.
+    def testGetTagEmptyRestApi(self):
+        # Get tag from db and make sure list has the 5 pre defined tags.
+        response = requests.get('http://%s:%s/tag' %(self.server_ip, self.server_port))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(eval(response.content)['tag1'], 'datacenter')
+        self.assertEqual(eval(response.content)['tag2'], 'floor')
+        self.assertEqual(eval(response.content)['tag3'], 'hall')
+        self.assertEqual(eval(response.content)['tag4'], 'rack')
+        self.assertEqual(eval(response.content)['tag5'], 'user_tag')
+    # end testGetTagEmptyRestApi
+
+    # Test case to test tag add REST API call.
+    def testAddTagRestApi(self):
+        # Add tags to DB, request adds 2 tags. Make sure response of add is ok.
+        # then get all tags and check response to confirm there are 7 tags.
+        with open('test_tag.json') as data_file:
+            tag_payload = data_file.read()
+        response = requests.put(
+            'http://%s:%s/tag' %(self.server_ip, self.server_port),
+            data=tag_payload,
+            headers = {'content-type': 'application/json'})
+        # Clusters should be added successfully.
+        self.assertEqual(response.status_code, 200)
+    # end testAddTagRestApi
+
+    # Test case to test tag get REST API call, after 2 tags are added to the DB.
+    def testGetTagAllRestApi(self):
+        response = requests.get('http://%s:%s/tag' %(self.server_ip, self.server_port))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 7)
+        tag_vals=[response.json()[each_tag] for each_tag in response.json()]
+        for tag_val in tag_vals:
+            self.assertTrue(tag_val in ['datacenter', 'floor', 'hall', 'rack', 'user_tag', 'test_tag_1', 'test_tag_2'])
+    # end testGetTagAllRestApi
+
+    # Test case to test tag modify REST API call
+    def testModifyTagRestApi(self):
+        # Modify one of the tag name to a invalid tag name and check that modification fails,
+        # with proper error message.
+        # Modify 2 of the tag values and do a get to check modification went through fine.
+        response = requests.get('http://%s:%s/tag' %(self.server_ip, self.server_port))
+        self.assertEqual(response.status_code, 200)
+        newdata = response.content
+
+        newdata = newdata.replace('tag7', 'tag8')
+        response = requests.put('http://%s:%s/tag' %(
+            self.server_ip, self.server_port),data=newdata,headers = {'content-type': 'application/json'})
+        self.assertEqual(response.status_code, 404)
+        self.assertTrue('Invalid tag tag8 specified' in response.content)
+
+        newdata = newdata.replace('tag8', 'tag7')
+        newdata = newdata.replace('test_tag_2', 'test_tag_value_new')
+        newdata = newdata.replace('hall', 'hall_value')
+        response = requests.put('http://%s:%s/tag' %(
+            self.server_ip, self.server_port),data=newdata,headers = {'content-type': 'application/json'})
+        self.assertEqual(response.status_code, 200)
+        response = requests.get('http://%s:%s/tag' %(
+                self.server_ip, self.server_port))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(eval(response.content)['tag7'], 'test_tag_value_new')
+        self.assertEqual(eval(response.content)['tag3'], 'hall_value')
+    # end testModifyTagRestApi
+
+    def testDeleteTagRestApi(self):
+        # Delete a tag, confirm result of delete REST and check
+        # the delete method should not be allowed on tags.
+        response = requests.delete('http://%s:%s/tag' %(self.server_ip, self.server_port))
+        self.assertEqual(response.status_code, 405)
+        self.assertTrue('405 Method Not Allowed' in response.content)
+    # end testDeleteTagRESTApi
+# end class TestSMTagRestApi
+
+#Create a TestSuite for the testing server manager rest api functionality
+def sm_tag_rest_api_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(TestSMTagRestApi('testGetTagEmptyRestApi'))
+    suite.addTest(TestSMTagRestApi('testAddTagRestApi'))
+    suite.addTest(TestSMTagRestApi('testGetTagAllRestApi'))
+    suite.addTest(TestSMTagRestApi('testModifyTagRestApi'))
+    suite.addTest(TestSMTagRestApi('testDeleteTagRestApi'))
+    return suite
 #Run the sm rest api testsuite.
 if __name__ == '__main__':
     mySuite = sm_rest_api_suite()
