6990d86bc12115a7b9064e326451a2d4e8bea2b0
Kamlesh Parmar <kparmar@juniper.net>
2016-10-05 23:01:37 -0700

    Partial-Bug: #1629495 separate out issu code into its class. Add issu_finalize function and Add compute migrate based on tag.
    Comment out server_mgr_issu import till corresponding packager change is merged
    
    Change-Id: I7d87ea6707d0f9b3449be302767ecb931b1c6c78

--- src/client/smgr_issu.py
+++ src/client/smgr_issu.py
@@ -5,7 +5,7 @@ import smgr_client_def
 from smgr_provision_server import *
 import ConfigParser
 
-def parse_arguments(args_str=None):
+def parse_arguments(args_str=None, flag = False):
     parser = argparse.ArgumentParser(
         description='''Perform in service software upgrade from old 
                        cluster to new cluster''',
@@ -19,18 +19,48 @@ def parse_arguments(args_str=None):
                         help=("cluster to be upgraded"))
     parser.add_argument("--cluster_id_new",
                         help=("active cluster after the upgrade"))
-    #parser.add_argument("--old_version",
-    #                    help=("version of the cluster being upgraded"))
-    parser.add_argument("--new_image",
+    if not flag:
+        parser.add_argument("--new_image",
                         help=("contrail image cluster being upgraded to"))
+    grp = parser.add_mutually_exclusive_group(required = False)
+    grp.add_argument("--tag", default = None,
+                        help=("compute nodes with specified tag are " +\
+                              "migrated from old cluster to new cluster."))
+    grp.add_argument("--all", dest = 'compute_all', action = "store_true",
+                        help=("compute nodes with specified tag are " +\
+                              "migrated from old cluster to new cluster."))
     parser.add_argument("--no_confirm", "-F", action="store_true",
                         help=("flag to bypass confirmation message, "
                               "default = do not bypass"))
     args = parser.parse_args(args_str)
     return args
 
-def do_issu(args_str=None):
-    args = parse_arguments(args_str)
+def do_issu_finalize(args_str = None):
+
+    args = parse_arguments(args_str, flag = True)
+    smgr_port, smgr_ip = setup_smgr(args)
+    # contruct paylod for backend
+    payload = {}
+    payload['opcode'] = 'issu_finalize'
+    payload['old_cluster'] = args.cluster_id_old
+    payload['new_cluster'] = args.cluster_id_new
+
+    if (not args.no_confirm):
+        msg = "Switch from cluster %s to %s? (y/N) :" %(
+                            args.cluster_id_old, args.cluster_id_new)
+        user_input = raw_input(msg).lower()
+        if user_input not in ["y", "yes"]:
+            sys.exit()
+    # end if
+
+    resp = send_REST_request(smgr_ip, smgr_port,
+                             payload)
+    smgr_client_def.print_rest_response(resp)
+
+# end do_issu_finalize
+
+def setup_smgr(args):
+
     if args.config_file:
         config_file = args.config_file
     else:
@@ -47,8 +77,13 @@ def do_issu(args_str=None):
         smgr_port = smgr_config.get("listen_port", smgr_client_def._DEF_SMGR_PORT)
     except:
         sys.exit("Error reading config file %s" %config_file)
-    # end except
 
+    return smgr_port, smgr_ip
+
+def do_issu(args_str=None):
+    args = parse_arguments(args_str)
+
+    smgr_port, smgr_ip = setup_smgr(args)
     # contruct paylod for backend
     payload = {}
     payload['opcode'] = 'issu'
@@ -56,11 +91,17 @@ def do_issu(args_str=None):
     payload['new_cluster'] = args.cluster_id_new
     #payload['old_version'] = args.old_version
     payload['new_image'] = args.new_image
+    if args.compute_all:
+        payload['compute_tag'] = "all_computes"
+    elif args.tag:
+        payload['compute_tag'] = args.tag
+    else:
+        payload['compute_tag'] = None
 
     if (not args.no_confirm):
         msg = "Upgrade cluster %s to %s, Contrail Image:%s? (y/N) :" %(
-                                args.cluster_id_old, args.cluster_id_new,
-                                args.new_image)
+                            args.cluster_id_old, args.cluster_id_new,
+                            args.new_image)
         user_input = raw_input(msg).lower()
         if user_input not in ["y", "yes"]:
             sys.exit()
