73c43708c028fcb45fbb3b4c7f25efa1d9579156
Ramprakash <rammohan@juniper.net>
2016-12-20 12:09:12 -0800

    Partial-bug: #1642373 Add support for provisioning docker containers. Consists of: 1. "server-manager add image" can now point to either         a. docker container tar.gz images present locally in  the SM node                 OR         b. a docker image in a docker registry following the            <server-ip>:<port>/<image-name>:<image-tag> format 2. support the configs required for the containers to be provisioned in the form of json data at either the cluster or the server level. 3. The role for each container could be one of "control"/"agent"/"analytics"/"analyticsdb"/"lb" 4. Add support to run ansible playbooks from within the "provision" workflow to    launch the containers. This uses the python multiprocess module to    parallelize the playbook runs. 5. Add new subcommand to the "display" command to get the log files and their    contents from the nodes where the containers are provisioned
    Change-Id: Ifb3b723112b12a2f84f07365761893b3084b1c2f

---
+++ src/ansible/sm_ansible_playbook.py
@@ -0,0 +1,192 @@
+import os
+import sys
+import multiprocessing
+import ConfigParser
+import tempfile
+from collections import namedtuple
+
+from ansible.parsing.dataloader import DataLoader
+from ansible.vars import VariableManager
+from ansible.inventory import Inventory
+from ansible.executor.playbook_executor import PlaybookExecutor
+
+from sm_ansible_utils import *
+
+""" 
+    wrapper class inspired from
+    http://docs.ansible.com/ansible/developing_api.html
+"""
+class ContrailAnsiblePlayBook(multiprocessing.Process):
+
+    STATUS_VALID = "Parameters_Valid"
+    STATUS_IN_PROGRESS = "In_Progress"
+    STATUS_FILE_NOT_FOUND = "Playbook_Path_Error"
+    STATUS_SUCCESS = "Provision_Success"
+    STATUS_FAILED  = "Provision_Failed"
+
+    def validate_provision_params(self, entity, defaults):
+
+        keys_to_check = ["ansible_playbook", "container_name", "ansible_host",
+                         "ansible_user", "ansible_password",
+                         "docker_insecure_registries",
+                         "docker_registry_insecure", "container_name",
+                         "container_image"]
+
+        params = entity.get("parameters", None)
+
+        for x in keys_to_check:
+            if not x in params.keys():
+                if x == "docker_insecure_registries":
+                    params['docker_insecure_registries'] = \
+                     defaults.docker_insecure_registries
+                elif x == 'docker_registry_insecure':
+                    params['docker_registry_insecure'] = \
+                     defaults.docker_registry_insecure
+                elif x == 'ansible_playbook':
+                    params['ansible_playbook'] = \
+                     defaults.ansible_playbook
+                else:
+                    return ("%s not defined in parameters" % x)
+
+        for k,v in vars(defaults).iteritems():
+            if not k in params.keys():
+                params[k] = v
+
+        pbook     = params['ansible_playbook']
+        try:
+            with open(pbook) as file:
+                pass
+        except IOError as e:
+            return ("Playbook not found : %s" % pbook)
+
+        return self.STATUS_VALID
+
+
+    def __init__(self, json_entity, args):
+        super(ContrailAnsiblePlayBook, self).__init__()
+        inv = {}
+        inv["[all:children]"] = []
+        controllers = []
+        analytics = []
+        analyticsdb = []
+        lb = []
+        agent = []
+        for params in json_entity:
+            srvrid = params.get("server_id", None)
+            parameters = params.get("parameters", None)
+            self.current_status = self.validate_provision_params(params, args)
+            self.srvrid             = srvrid
+            self.server             = parameters["ansible_host"]
+            self.pbook_path         = parameters["ansible_playbook"]
+            self.role               = parameters["container_name"]
+            pbook_dir = os.path.dirname(self.pbook_path)
+            rl = self.role
+            if self.role == 'control':
+                rl = 'controller'
+                inv["[contrail-controllers]"] = self.server
+                if "contrail-controllers" not in inv["[all:children]"]:
+                    inv["[all:children]"].append("contrail-controllers")
+            elif self.role == 'analytics':
+                inv["[contrail-analytics]"] = self.server
+                if "contrail-analytics" not in inv["[all:children]"]:
+                    inv["[all:children]"].append("contrail-analytics")
+            elif self.role == 'analyticsdb':
+                inv["[contrail-analyticsdb]"] = self.server
+                if "contrail-analyticsdb" not in inv["[all:children]"]:
+                    inv["[all:children]"].append("contrail-analyticsdb")
+            elif self.role == 'agent':
+                inv["[contrail-compute]"] = self.server
+                if "contrail-compute" not in inv["[all:children]"]:
+                    inv["[all:children]"].append("contrail-compute")
+            elif self.role == 'lb':
+                inv["[contrail-lb]"] = self.server
+                if "contrail-lb" not in inv["[all:children]"]:
+                    inv["[all:children]"].append("contrail-lb")
+         
+            params["config_file_dest"]   = '/etc/contrailctl/' + rl + '.conf'
+            params["config_file_src"]    = pbook_dir + '/contrailctl/' + rl + '.conf'
+            print "config file is %s" % params["config_file_dest"]
+         
+            if self.current_status == self.STATUS_VALID:
+                create_conf_file(params["config_file_src"], 
+                                 parameters[parameters["container_name"]])
+
+        inv_dir = pbook_dir + '/inventory/'
+        inv_file = \
+            tempfile.NamedTemporaryFile(dir=inv_dir, delete=False).name
+        create_inv_file(inv_file, inv)
+            
+        self.var_mgr            = VariableManager()
+        self.ldr                = DataLoader()
+        self.var_mgr.extra_vars = params['parameters']
+        self.args               = args
+        self.inventory          = Inventory(loader=self.ldr, 
+                                            variable_manager=self.var_mgr,
+                                            host_list=inv_file)
+        self.var_mgr.set_inventory(self.inventory)
+
+        
+        Options = namedtuple('Options', ['connection', 'forks', 'module_path',
+                             'become', 'become_method', 'become_user', 'check',
+                             'listhosts', 'listtasks', 'listtags', 'syntax',
+                             'verbosity'])
+        self.options = Options(connection='ssh', forks=100, module_path=None, 
+                               become=True,
+                               become_method='sudo', become_user='root',
+                               check=False, listhosts=None, listtasks=None,
+                               listtags=None, syntax=None, verbosity=None)
+
+        self.pws         = {}
+        self.pb_executor = PlaybookExecutor(playbooks=[self.pbook_path],
+                                            inventory=self.inventory,
+                                            variable_manager=self.var_mgr,
+                                            loader=self.ldr,
+                                            options=self.options, 
+                                            passwords=self.pws)
+
+    def run(self):
+        #import pdb; pdb.set_trace()
+        if self.current_status == self.STATUS_VALID: 
+            self.current_status = self.STATUS_IN_PROGRESS
+            status_resp = { "server_id" : self.srvrid, 
+                            "role" : self.role,
+                            "state" : self.current_status }
+            send_REST_request(self.args.ansible_srvr_ip,
+                              self.args.ansible_srvr_port, 
+                              "playbook_status", status_resp, method='PUT')
+            rv = self.pb_executor.run()
+            print "RUN DONE"
+            stats = self.pb_executor._tqm._stats
+
+            run_success = True
+            hosts = sorted(stats.processed.keys())
+            for h in hosts:
+                t = stats.summarize(h)
+                if t['unreachable'] > 0 or t['failures'] > 0:
+                    run_success = False
+
+            # send callback to the function "record_logs" in the callback 
+            # plugin in the plugins directory
+            self.pb_executor._tqm.send_callback('record_logs')
+
+            if rv == 0:
+                self.current_status = self.STATUS_SUCCESS
+            else:
+                self.current_status = self.STATUS_FAILED
+
+            status_resp = { "server_id" : self.srvrid, 
+                            "role" : self.role,
+                            "state" : self.current_status }
+            send_REST_request(self.args.ansible_srvr_ip,
+                    self.args.ansible_srvr_port, 
+                              "playbook_status", status_resp, method='PUT')
+            print self.current_status
+            return stats
+        else:
+            print "Validation Failed"
+            status_resp = { "server_id" : self.srvrid, 
+                            "role" : self.role,
+                            "state" : self.current_status }
+            send_REST_request(self.args.ansible_srvr_ip,
+                    self.args.ansible_srvr_port, 
+                              "playbook_status", status_resp, method='PUT')
