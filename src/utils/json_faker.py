6ca354c01c081826a24bcc5cfca775c6191ae1bf
Rasika Dravid <rdravid@juniper.net>
2017-07-06 12:03:20 -0700

    Closes-Bug: 1701397 - This tool validates the JSONs from JSON schema
    Sample command which can be used to run this tool is as below :
    
    ./json_faker.py -s <JSON-schema files under this path> -v <JSON file to be validated>
    
    -s this option takes argument as an input file
    -v this option takes argument as a file to be validated
    
    The Validation Messages are displayed on user screen
    
    Change-Id: Ibf07f92b591cdfee8897f2489bcabf03ec8684aa

--- src/utils/json_faker.py
+++ src/utils/json_faker.py
@@ -3,6 +3,31 @@ import sys
 import json
 import argparse
 import optparse
+import os
+from jsonschema import validate, RefResolver,FormatChecker, exceptions
+
+key_translations = {
+   "agent_config" : "AGENT",
+   "schema_config" : "SCHEMA",
+   "alarm_gen_config" : "ALARM_GEN",
+   "device_manager_config" : "DEVICE_MANAGER",
+   "analytics_api_config" : "ANALYTICS_API",
+   "topology_config" : "TOPOLOGY",
+   "snmp_collector_config" : "SNMP_COLLECTOR",
+   "query_engine_config" : "QUERY_ENGINE",
+   "cassandra_config" : "CASSANDRA",
+   "keystone_config" : "KEYSTONE",
+   "global_config" : "GLOBAL",
+   "svc_monitor_config" : "SVC_MONITOR",
+   "hypervisor_config" : "HYPERVISOR",
+   "control_config" : "CONTROL",
+   "analytics_collector_config" : "ANALYTICS_COLLECTOR",
+   "rabbitmq_config" : "RABBITMQ",
+   "webui_config" : "WEBUI",
+   "dns_config" : "DNS",
+   "controller_config" : "CONTROLLER",
+   "api_config" : "API"
+}
 
 def load_schema(schema_file):
     data = None
@@ -25,7 +50,7 @@ def handle_type(schema, final_json, last_key):
     elif schema['type'] == 'array':
         final_json[last_key] = []
 
-#TODO: handle ref by calling generate_data with appropriate dict loaded from
+# handle ref by calling generate_data with appropriate dict loaded from
 # section of the $ref file
 def handle_ref(defs, schema, final_json, last_key):
     ref_str = schema["$ref"].split("/")[-1]
@@ -57,23 +82,7 @@ def generate_data(defs, schema, final_json, last_key=None):
             final_json[k] = dict()
             generate_data(defs, schema['properties'], final_json[k], k)
 
-
-def main():
-    final_json = {}
-    defs = {}
-    final_json['contrail_4'] = {}
-
-    OptionParser = optparse.OptionParser()
-    OptionParser.add_option('-i', '--input',action="append", dest="inputfiles",help="inputfile string", default=[])
-    OptionParser.add_option('-o', '--output',action="store", dest="output",help="outputfile string", default=[])
-    options, args = OptionParser.parse_args()
-
-    samplefile = options.output
-
-    if len(sys.argv) == 1:
-        print "USAGE: %s [-h][-i] <path-to-schema-jsons> [-o] <path-to-generated-json>" % sys.argv[0]
-
-
+def created_JSON(options,final_json):
     for inputOP in options.inputfiles:
         if "definitions.json" in inputOP:
             defs = load_schema(inputOP)
@@ -84,8 +93,84 @@ def main():
             if 'properties' in data.keys():
                 generate_data(defs, data['properties'], final_json['contrail_4'])
 
-    with open(samplefile, 'w') as outfile:
+    with open(options.output, 'w') as outfile:
         json.dump(final_json, outfile, indent = 2)
+
+def combine_dictionaries(dict1, dict2):
+    final_output = {}
+    for item, value in dict1.iteritems():
+        if dict2.has_key(item):
+            if isinstance(dict2[item], dict):
+                final_output[item] = combine_dictionaries(value, dict2.pop(item))
+        else:
+            final_output[item] = value
+    for item, value in dict2.iteritems():
+         final_output[item] = value
+    return final_output
+
+def validate_JSON(data,combined_schema):
+    for keys in combined_schema['properties']:
+        if "$ref" in (combined_schema['properties'])[keys]:
+            refUrl= ((combined_schema['properties'])[keys])['$ref']
+            references_file, section = refUrl.split('#')
+
+    if not os.path.isfile(references_file):
+        print "References file does not exists"
+        sys.exit()
+
+    schema_dir = os.path.dirname(os.path.realpath(references_file))
+    resolver = RefResolver("file://{}/".format(schema_dir), None)
+
+    try:
+        validate(data,combined_schema,format_checker=FormatChecker(), resolver=resolver)
+        print "JSON is valid with the schema"
+        return True
+    except exceptions.ValidationError as error:
+        print(error.message)
+        return False
+
+def pre_validation(options, fschema_json):
+    for inputOP in options.inputfiles:
+        if "definitions.json" not in inputOP:
+            with open(inputOP, "r") as infile:
+                fschema_json = combine_dictionaries(fschema_json, json.load(infile))
+
+    merged_schema = json.dumps(fschema_json, indent = 2)
+    s = open(options.validateIt,'r').read()
+
+    for key in key_translations:
+        s = s.replace(key, key_translations[key])
+    dict_to_validate = json.loads(s)
+
+    if 'contrail_4' in dict_to_validate.keys():
+        dict_to_validate = dict_to_validate['contrail_4']
+
+    schema_dict =(json.loads(merged_schema))
+    validate_JSON(dict_to_validate, schema_dict)
+
+def main():
+    final_json = {}
+    fschema_json = {}
+    defs = {}
+    final_json['contrail_4'] = {}
+
+    OptionParser = optparse.OptionParser()
+    OptionParser.add_option('-s', '--input',action="append", dest="inputfiles",help="inputfile string", default=[])
+    OptionParser.add_option('-o', '--output',action="store", dest="output",help="outputfile string", default=[])
+    OptionParser.add_option('-v', '--validate',action="store", dest="validateIt",help="JSON file to be validated", default=[])
+    options, args = OptionParser.parse_args()
+
+    if len(sys.argv) == 1:
+        print "USAGE: %s [-h][-s] <path-to-schema-jsons> [-o] <path-to-generated-json> [-v] <path-to-json-to-be-validated>" % sys.argv[0]
+
+    elif options.output:
+        created_JSON(options,final_json)
+
+    elif options.validateIt:
+        pre_validation(options,fschema_json)
+
+    else:
+        sys.exit()
 
 if __name__ == "__main__":
     main()
