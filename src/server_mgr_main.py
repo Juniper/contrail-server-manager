dd4cfffe8764d56acec530a2c284095552fde3fe
nitishkrishna <nitishk@juniper.net>
2017-09-25 10:03:23 -0700

    Closes-Bug: #1719256 - Need to import collections for OrderedDict
    Missed out importing in previous check-in for provision status
    
    Change-Id: If03a39f30f99c80f3dac2c48dc0b45539d5fec15

--- src/server_mgr_main.py
+++ src/server_mgr_main.py
@@ -95,7 +95,10 @@ from StringIO import StringIO
 import requests
 import tempfile
 from contrail_defaults import *
-
+try:
+    from collections import OrderedDict
+except ImportError:
+    from ordereddict import OrderedDict
 from gevent import monkey
 monkey.patch_all()
 import gevent
@@ -1445,7 +1448,7 @@ class VncServerManager():
                     if not len(provisioned_roles_dict.keys()):
                         msg =  ("Server with Id %s is currently not under provision." % (server['id']))
                         self.log_and_raise_exception(msg)
-                    provision_server_dict = collections.OrderedDict((('id', str(server['host_name'])),
+                    provision_server_dict = OrderedDict((('id', str(server['host_name'])),
                         ('cluster_id', str(server['cluster_id'])),
                         ('provision_pending', str(server_params['provisioned_roles']['roles_to_provision'])),
                         ('provision_in_progress', str(server_params['provisioned_roles']['role_under_provision'])),
